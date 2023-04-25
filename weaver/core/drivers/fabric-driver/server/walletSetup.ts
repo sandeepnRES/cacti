edsonCVN/cacti-edson-fork

refactor(weaver): prefix cacti to all weaver packages, set publish target to cacti
    - Update relay, drivers, iin-agent image registry to hyperledger/cacti
    - Update npm packages (protos-js and sdks) registry to cacti
    - Update maven packages registry to cacti
    - Rename sdks/besu/interoperation-node-sdk to sdks/besu/node
    - Rename modules with following convention `<module-type>-<dlt-platform>`:
        - For SDKs: `sdk.corda`, `sdk-fabric`, `sdk-besu`
        - For drivers: `driver-corda`, `driver-fabric`
        - For imodule: `imodule.corda`
    - Change all packages version to v2.0.0-alpha-prerelease

Signed-off-by: Sandeep Nishad <sandeep.nishad1@ibm.com>

weaver/core/drivers/fabric-driver/server/walletSetup.ts
@@ -7,7 +7,7 @@
 import { Wallets } from 'fabric-network';
 import FabricCAServices from 'fabric-ca-client';
 import { handlePromise } from './utils';
-import { InteroperableHelper } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { InteroperableHelper } from '@hyperledger/cacti-weaver-sdk-fabric'
 import * as path from 'path';
 import * as fs from 'fs';
 import logger from './logger';

weaver/core/identity-management/iin-agent/src/common/utils.ts
@@ -12,10 +12,10 @@ import crypto from 'crypto';
 const { X509Certificate } = require('crypto');
 
 // Weaver Packages
-import agent_grpc_pb from '@hyperledger-labs/weaver-protos-js/identity/agent_grpc_pb';
-import agent_pb from '@hyperledger-labs/weaver-protos-js/identity/agent_pb';
-import membership_pb from '@hyperledger-labs/weaver-protos-js/common/membership_pb';
-import { InteroperableHelper } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import agent_grpc_pb from '@hyperledger/cacti-weaver-protos-js/identity/agent_grpc_pb';
+import agent_pb from '@hyperledger/cacti-weaver-protos-js/identity/agent_pb';
+import membership_pb from '@hyperledger/cacti-weaver-protos-js/common/membership_pb';
+import { InteroperableHelper } from '@hyperledger/cacti-weaver-sdk-fabric'
 
 // Local modules
 import { LedgerBase } from './ledgerBase';

weaver/samples/fabric/fabric-cli/src/commands/asset/exchange/claim.ts
@@ -8,7 +8,7 @@ import { GluegunCommand } from 'gluegun'
 import { fabricHelper } from '../../../helpers/fabric-functions'
 import logger from '../../../helpers/logger'
 import { commandHelp, getNetworkConfig, handlePromise } from '../../../helpers/helpers'
-import { AssetManager, HashFunctions } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { AssetManager, HashFunctions } from '@hyperledger/cacti-weaver-sdk-fabric'
 
 const command: GluegunCommand = {
   name: 'claim',

weaver/samples/fabric/fabric-cli/src/commands/asset/exchange/is-locked.ts
@@ -8,7 +8,7 @@ import { GluegunCommand } from 'gluegun'
 import { fabricHelper } from '../../../helpers/fabric-functions'
 import logger from '../../../helpers/logger'
 import { commandHelp, getNetworkConfig, handlePromise } from '../../../helpers/helpers'
-import { AssetManager } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { AssetManager } from '@hyperledger/cacti-weaver-sdk-fabric'
 
 const command: GluegunCommand = {
   name: 'is-locked',

weaver/samples/fabric/fabric-cli/src/commands/asset/exchange/lock.ts
@@ -8,7 +8,7 @@ import { GluegunCommand } from 'gluegun'
 import { fabricHelper } from '../../../helpers/fabric-functions'
 import logger from '../../../helpers/logger'
 import { commandHelp, getNetworkConfig, handlePromise } from '../../../helpers/helpers'
-import { AssetManager, HashFunctions } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { AssetManager, HashFunctions } from '@hyperledger/cacti-weaver-sdk-fabric'
 
 const command: GluegunCommand = {
   name: 'lock',

weaver/samples/fabric/fabric-cli/src/commands/configure/membership.ts
@@ -10,7 +10,7 @@ import logger from '../../helpers/logger'
 import * as path from 'path'
 import * as dotenv from 'dotenv'
 import * as fs from 'fs'
-import { MembershipManager } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { MembershipManager } from '@hyperledger/cacti-weaver-sdk-fabric'
 dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
 const command: GluegunCommand = {

weaver/samples/fabric/fabric-cli/src/commands/hash-helper.ts
@@ -7,7 +7,7 @@
 import { GluegunCommand } from 'gluegun'
 import logger from '../helpers/logger'
 import { commandHelp } from '../helpers/helpers'
-import { HashFunctions } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { HashFunctions } from '@hyperledger/cacti-weaver-sdk-fabric'
 
 const command: GluegunCommand = {
   name: 'hash',

sandeepnRES/cacti

weaver/core/drivers/fabric-driver/server/walletSetup.ts
@@ -7,7 +7,7 @@
 import { Wallets } from 'fabric-network';
 import FabricCAServices from 'fabric-ca-client';
 import { handlePromise } from './utils';
-import { InteroperableHelper } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { InteroperableHelper } from '@hyperledger/cacti-weaver-fabric-sdk'
 import * as path from 'path';
 import * as fs from 'fs';
 import logger from './logger';

weaver/core/identity-management/iin-agent/src/common/utils.ts
@@ -12,10 +12,10 @@ import crypto from 'crypto';
 const { X509Certificate } = require('crypto');
 
 // Weaver Packages
-import agent_grpc_pb from '@hyperledger-labs/weaver-protos-js/identity/agent_grpc_pb';
-import agent_pb from '@hyperledger-labs/weaver-protos-js/identity/agent_pb';
-import membership_pb from '@hyperledger-labs/weaver-protos-js/common/membership_pb';
-import { InteroperableHelper } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import agent_grpc_pb from '@hyperledger/cacti-weaver-protos-js/identity/agent_grpc_pb';
+import agent_pb from '@hyperledger/cacti-weaver-protos-js/identity/agent_pb';
+import membership_pb from '@hyperledger/cacti-weaver-protos-js/common/membership_pb';
+import { InteroperableHelper } from '@hyperledger/cacti-weaver-fabric-sdk'
 
 // Local modules
 import { LedgerBase } from './ledgerBase';

weaver/samples/fabric/fabric-cli/src/commands/asset/exchange/claim.ts
@@ -8,7 +8,7 @@ import { GluegunCommand } from 'gluegun'
 import { fabricHelper } from '../../../helpers/fabric-functions'
 import logger from '../../../helpers/logger'
 import { commandHelp, getNetworkConfig, handlePromise } from '../../../helpers/helpers'
-import { AssetManager, HashFunctions } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { AssetManager, HashFunctions } from '@hyperledger/cacti-weaver-fabric-sdk'
 
 const command: GluegunCommand = {
   name: 'claim',

weaver/samples/fabric/fabric-cli/src/commands/asset/exchange/is-locked.ts
@@ -8,7 +8,7 @@ import { GluegunCommand } from 'gluegun'
 import { fabricHelper } from '../../../helpers/fabric-functions'
 import logger from '../../../helpers/logger'
 import { commandHelp, getNetworkConfig, handlePromise } from '../../../helpers/helpers'
-import { AssetManager } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { AssetManager } from '@hyperledger/cacti-weaver-fabric-sdk'
 
 const command: GluegunCommand = {
   name: 'is-locked',

weaver/samples/fabric/fabric-cli/src/commands/asset/exchange/lock.ts
@@ -8,7 +8,7 @@ import { GluegunCommand } from 'gluegun'
 import { fabricHelper } from '../../../helpers/fabric-functions'
 import logger from '../../../helpers/logger'
 import { commandHelp, getNetworkConfig, handlePromise } from '../../../helpers/helpers'
-import { AssetManager, HashFunctions } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { AssetManager, HashFunctions } from '@hyperledger/cacti-weaver-fabric-sdk'
 
 const command: GluegunCommand = {
   name: 'lock',

weaver/samples/fabric/fabric-cli/src/commands/configure/membership.ts
@@ -10,7 +10,7 @@ import logger from '../../helpers/logger'
 import * as path from 'path'
 import * as dotenv from 'dotenv'
 import * as fs from 'fs'
-import { MembershipManager } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { MembershipManager } from '@hyperledger/cacti-weaver-fabric-sdk'
 dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
 const command: GluegunCommand = {

weaver/samples/fabric/fabric-cli/src/commands/hash-helper.ts
@@ -7,7 +7,7 @@
 import { GluegunCommand } from 'gluegun'
 import logger from '../helpers/logger'
 import { commandHelp } from '../helpers/helpers'
-import { HashFunctions } from '@hyperledger-labs/weaver-fabric-interop-sdk'
+import { HashFunctions } from '@hyperledger/cacti-weaver-fabric-sdk'
 
 const command: GluegunCommand = {
   name: 'hash',